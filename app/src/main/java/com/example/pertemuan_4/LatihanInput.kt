8402eaef39c520d5edbdf599fd92e06995955ce9
kyy08 <kyyelzz1@gmail.com>
2024-10-28 19:22:46 +0700

    membenarkan eror dan menambahkan yg kurang (soalnya tadi laptopnya eror mas)

--- app/src/main/java/com/example/pertemuan_4/LatihanInput.kt
+++ app/src/main/java/com/example/pertemuan_4/LatihanInput.kt
@@ -1,11 +1,14 @@
 package com.example.pertemuan_4
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -24,17 +27,19 @@ import java.util.NavigableMap
 @Preview(showBackground = true)
 @Composable
 fun LatihanInput(modifier: Modifier = Modifier) {
-
     var nama  by remember { mutableStateOf("") }
     var email  by remember { mutableStateOf("") }
     var alamat by remember { mutableStateOf("") }
     var notelepon by remember { mutableStateOf("") }
+    var gender by remember { mutableStateOf("") }
 
-    var data
-    var confnama  by remember { mutableStateOf("") }
-    var confemail  by remember { mutableStateOf("") }
-    var confalamat by remember { mutableStateOf("") }
-    var confnotelepon by remember { mutableStateOf("") }
+    var dataGender = listOf("Laki-Laki","Perempuan")
+    
+    var confNama  by remember { mutableStateOf("") }
+    var confEmail  by remember { mutableStateOf("") }
+    var confAlamat by remember { mutableStateOf("") }
+    var confNotelepon by remember { mutableStateOf("") }
+    var confGender by remember { mutableStateOf("") }
 
 
     Column(
@@ -50,8 +55,19 @@ fun LatihanInput(modifier: Modifier = Modifier) {
             placeholder = {
                 Text(text = "isi nama anda")
             },
-            modifier= Modifier.fillMaxWidth()
+            modifier= Modifier.fillMaxWidth().padding(5.dp)
         )
+        Row(modifier = Modifier.fillMaxWidth()) {
+            dataGender.forEach{selectedGender->
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    RadioButton(
+                        selected = gender == selectedGender,
+                        onClick = {gender = selectedGender}
+                    )
+                    Text(text = selectedGender)}
+
+            }
+        }
         TextField(
             value = email,
             onValueChange = {email = it},
@@ -61,7 +77,7 @@ fun LatihanInput(modifier: Modifier = Modifier) {
             placeholder = {
                 Text(text = "isi email anda")
             },
-            modifier= Modifier.fillMaxWidth(),
+            modifier= Modifier.fillMaxWidth().padding(5.dp),
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
         )
         TextField(
@@ -73,7 +89,7 @@ fun LatihanInput(modifier: Modifier = Modifier) {
             placeholder = {
                 Text(text = "isi alamat anda")
             },
-            modifier= Modifier.fillMaxWidth()
+            modifier= Modifier.fillMaxWidth().padding(5.dp)
         )
         TextField(
             value = notelepon,
@@ -84,19 +100,50 @@ fun LatihanInput(modifier: Modifier = Modifier) {
             placeholder = {
                 Text(text = "isi notelepon anda")
             },
-            modifier= Modifier.fillMaxWidth(),
+            modifier= Modifier.fillMaxWidth().padding(5.dp),
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
         )
         Button(onClick = {
-            confnama = nama
-            confemail = email
-            confalamat = alamat
-            confnotelepon = notelepon
+            confNama = nama
+            confEmail = email
+            confAlamat = alamat
+            confNotelepon = notelepon
+            confGender = gender
         }) {
-
+            Text(text = "Simpan")
         }
+        TampilData(
+            Param = "Nama",
+            Argu = confNama
+        )
+        TampilData(
+            Param = "Email",
+            Argu = confEmail
+        )
+        TampilData(
+            Param = "Alamat",
+            Argu = confAlamat
+        )
+        TampilData(
+            Param = "No Telepon",
+            Argu = confNotelepon
+        )
+        TampilData(
+            Param = "Gender",
+            Argu = confGender
+        )
 
+    }
+}
 
-
+@Composable
+fun TampilData(Param : String, Argu : String){
+    Column(modifier = Modifier.padding(16.dp)) {
+        Row(modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween){
+            Text(text = Param, modifier = Modifier.weight(0.8f))
+            Text(text = ":", modifier = Modifier.weight(0.2f))
+            Text(text = Argu, modifier = Modifier.weight(2f))
+        }
     }
 }
